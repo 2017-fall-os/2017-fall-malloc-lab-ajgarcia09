d11db9427188b9512b16e16468cf4ee570d122b9
ajgarcia09 <ajgarciaramirez@miners.utep.edu>
2017-12-17 16:59:02 -0700

    added code for testing bestFitAllocRegion

--- myAllocatorTest1.c
+++ myAllocatorTest1.c
@@ -17,8 +17,8 @@ void getutime(struct timeval *t)
   *t = usage.ru_utime;
 }
 
-int main() 
-{
+//Dr. Freudenthal's original testing code
+void testFirstFitAllocRegion(){
   void *p1, *p2, *p3;
   arenaCheck();
   p1 = firstFitAllocRegion(254);
@@ -34,18 +34,41 @@ int main()
   arenaCheck();
   freeRegion(p1);
   arenaCheck();
+}
+
+/*My own testing code for my allocation algorithm.
+Need to check the arena after every allocation*/
+void testBestFitAllocRegion(){
+  void *p1, *p2, *p3, *p4, *p5;
+  arenaCheck();
+  p1 = bestFitAllocRegion(32);
+  arenaCheck();
+  p2 = bestFitAllocRegion(32);
+  arenaCheck();
+  p3 = bestFitAllocRegion(32);
+  arenaCheck();
+  p4 = bestFitAllocRegion(32);
+  arenaCheck();
+
+  p5 = bestFitAllocRegion(32);
+  arenaCheck();
+
+  resizeRegion(p5, 40); //resoze the original size 32 region to a size 40
+  arenaCheck(); //always check the arena after allocating, resizing
+}
+
+int main()
+{
+testBestFitAllocRegion();
   {				/* measure time for 10000 mallocs */
     struct timeval t1, t2;
     int i;
     getutime(&t1);
     for(i = 0; i < 10000; i++)
-      if (firstFitAllocRegion(4) == 0) 
+      if (firstFitAllocRegion(4) == 0)
 	break;
     getutime(&t2);
-    printf("%d firstFitAllocRegion(4) required %f seconds\n", i, diffTimeval(&t2, &t1));
+    printf("%d bestFitAllocRegion() required %f seconds\n", i, diffTimeval(&t2, &t1));
   }
   return 0;
 }
-
-
-
